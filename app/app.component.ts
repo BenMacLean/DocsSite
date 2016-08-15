75755c0304ffd99d3762def2bf683972ff929b4b
joncodo <jonathan.carl.odonnell@gmail.com>
2016-08-15 08:20:53 -0300

    first run

---
+++ app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+import { HomeComponent } from './home/home.component';
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html',
+    directives: [HomeComponent]
+})
+export class AppComponent {
+  title = 'Jonathan';
+}
